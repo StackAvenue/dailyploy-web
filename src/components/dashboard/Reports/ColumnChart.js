f325935f432ac2db13e6880fc2c1d8371d699de8
Ravindra Karale <ravindra@stack-avenue.com>
2020-02-05 17:54:29 +0530

    column report issue fix

--- src/components/dashboard/Reports/ColumnChart.js
+++ src/components/dashboard/Reports/ColumnChart.js
@@ -213,7 +213,7 @@ class ColumnChart extends Component {
       var dailyScheduled = [];
       var dailyWorked = [];
       newSortedData.map(option => {
-        if (option.totalEstimateTime && option.trackedTime != 0) {
+        if (option.totalEstimateTime && option.trackedTime >= 0) {
           var total = this.secondsToHours(option.totalEstimateTime);
           if (option.totalEstimateTime >= option.trackedTime) {
             var scheduleDiff = option.totalEstimateTime - option.trackedTime;
@@ -307,7 +307,7 @@ class ColumnChart extends Component {
       var weeklyScheduled = [];
       var weeklyWorked = [];
       newSortedData.map(option => {
-        if (option.totalEstimateTime && option.trackedTime != 0) {
+        if (option.totalEstimateTime && option.trackedTime >= 0) {
           var total = this.secondsToHours(option.totalEstimateTime);
           if (option.totalEstimateTime >= option.trackedTime) {
             var scheduleDiff = option.totalEstimateTime - option.trackedTime;
@@ -407,7 +407,7 @@ class ColumnChart extends Component {
       var monthlyScheduled = [];
       var monthlyWorked = [];
       newSortedData.map(option => {
-        if (option.totalEstimateTime && option.trackedTime != 0) {
+        if (option.totalEstimateTime && option.trackedTime >= 0) {
           var total = this.secondsToHours(option.totalEstimateTime);
           if (option.totalEstimateTime >= option.trackedTime) {
             var scheduleDiff = option.totalEstimateTime - option.trackedTime;
