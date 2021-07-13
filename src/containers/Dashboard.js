c10b860612b7ec192a3a0fe9f12c901942e08df6
Sunil D N <sunil.dn.arya@gmail.com>
2021-07-13 13:22:42 +0530

    simplifing input handler function

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1623,17 +1623,14 @@ class Dashboard extends PureComponent {
     const { name, value } = e.target;
     var errors = this.state.errors;
     errors[`${name}Error`] = "";
-    if (name === "estimate") {
-      if (value.split(".")[0] === "" &&  Number(value) > 0) {
-        let newValue = "0" + value
-        this.setState({ [name]: newValue, errors: errors });
-      } else if (value === "0") {
-        let newValue =""
-        this.setState({ [name]: newValue, errors: errors });
-      } else {
-        this.setState({ [name]: value, errors: errors });
-      }
-    } else this.setState({ [name]: value, errors: errors });
+    let newValue = value;
+    if(name === "estimate" && value === "0"){
+      newValue = "";
+    }
+    else if(name === "estimate" && value.split(".")[0] === "" &&  Number(value) > 0){
+      newValue = "0" + value;
+    }
+    this.setState({ [name]: newValue, errors: errors });
   };
 
   handleProjectSelect = async (option) => {
